68997bba93fd0311f3e8e34226b9be052a69a884
k-kat358 <samir.sadee011@gmail.com>
2025-06-01 14:22:28 +0600

    Commit 11

--- djangoProject2/guides/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Guides, GuidesImages
-
-admin.site.register(Guides)
--- djangoProject2/products/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_item(dictionary, key):
-    """Template filter to access dictionary values by key"""
-    return dictionary.get(key, [])
